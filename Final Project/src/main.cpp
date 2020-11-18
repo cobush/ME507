7b0cdf22cbbd6b3eef4e1c14f5b40325308ef265
Shbushka <connor.bush97@gmail.com>
2020-11-18 09:36:20 -0800

    A lot happened here. see below
    Added MPU driver - includes 3 h files: MPU9250, MPU9250Registers, QuaternioinFilter
    
    Added PID class- MiniPID
    
    Updated main code to collect data and pass transition between tasks after completing a task to speed operation of control system. Not feasible for complex projects but will work for our case.

--- Final Project/src/main.cpp
+++ Final Project/src/main.cpp
@@ -4,6 +4,9 @@
 #include <math.h>
 
 #include <MPU9250.h>
+#include <MPU9250RegisterMap.h>
+#include <QuaternionFilter.h>
+#include <MiniPID.h>
 #include <PrintStream.h>
 #if (defined STM32L4xx || defined STM32F4xx)
     #include <STM32FreeRTOS.h>
@@ -13,98 +16,34 @@
 #include <taskshare.h>
 #include <baseshare.h>
 #include <task.h>
-#include "Controller.h"
 
 // define general variables to be used when pulling data from shares
 bool one_bit;
-uint8_t eight_bit;
+float float_val;
 uint16_t sixteen_bit;
 
-// define variables to control Cube reaction
-uint16_t mtr_duty;
+// PID, PWM and IMU Variables
+uint8_t mtr_duty;
 bool mtr_dir;
-uint16_t cube_ang;
-uint16_t imu;
-uint16_t generic_speed;
-uint16_t enca_i;
-uint16_t enca_f;
-uint16_t enca_diff;
-uint16_t time_diff;
+float cube_ang;
+// uint16_t imu;
+// uint16_t generic_speed;
+uint16_t error;
+uint8_t MAX_DUTY = 255; // Maximum duty cycle output
+double VERTICAL = 45; // constant value set to zero the gyro on the vertical axis
+double P = 1; // Proportional constant
+double I = 1; // Integral constant
+double D = 1; // Derivative constant
+double F = 0; // Feed forward value
 
 // Shares and queues go here
-Share<uint8_t> duty_cycle ("Power");
-Share<bool> direction ("Direction");
-Queue<uint16_t [6]> gyro_data ("Current Gyro Data");
-Share<uint16_t> motor_speed("Current Motor Speed");
-Share<uint8_t> cube_angle ("Current Cube Angle off Vertical");
-Share<uint8_t> state ("Current state of the ");
-
-
-void PWM (void* p_params)
-{
-(void)p_params;
-
-duty_cycle.get(eight_bit);
-mtr_duty = eight_bit;
-direction.get(one_bit);
-mtr_dir = one_bit;
-
-analogWrite(XX, mtr_duty);
-digitalWrite(XX, mtr_dir);
-
-}
-void PID ( void* p_params)
-{
-(void)p_params;
-uint16_t mtr_duty;
-duty_cycle.put(mtr_duty);
-}
-void Calculator (void* p_params)
-{
-(void)p_params;
-// Collect the current data
-gyro_data.get(sixteen_bit);
-imu = sixteen_bit;
-motor_speed.get(sixteen_bit);
-generic_speed = sixteen_bit;
-
-// Calculate motor direction and cube angle to pass into PID
-direction.put(mtr_dir);
-cube_angle.put(cube_ang);
-}
-void Encoder (void* p_params)
-{
-(void)p_params;
-
-// check size of timer variable
-if (0 == configUSE_16_BIT_TICKS)
-{
-
-}
-else
-{
-  /* code */
-}
-
-// collect data to calculate motor speed
-TickType_t time_i, time_f;
-time_i = xTaskGetTickCount();
-enca_i = analogRead(XX);
-time_f = xTaskGetTickCount();
-enca_f = analogRead(XX);
-
-// need two if-else clauses to caluclate magnitude of difference in encoder count
-// and timer count that does not wack out when timer or encoder cycle through zero
-generic_speed = (enca_diff)/(time_diff);
-
-motor_speed.put(generic_speed);
-}
-void IMU(void* p_params)
-{
-(void)p_params;
-
-}
-
+Share<uint8_t> duty_cycle ("Motor duty cycle");
+Share<bool> direction ("Motor direction");
+// Share<uint16_t> gyro_data ("Current Cube Rotation");
+// Share<uint16_t> motor_speed("Current Motor Speed");
+// Share<uint16_t> cube_speed("Current Cube Speed");
+Share<float> cube_angle ("Current cube angle off horizontal");
+// Share<uint8_t> state ("Current state of the ");
 
 void setup() 
 {
@@ -117,7 +56,12 @@ void setup()
     delay (2000);
     Serial << endl << endl << "ME507 Cube Balance Intializing..." << endl;
 
-    // Initialize the motor, encoder, and IMU
+    // Test the IMU connection 
+    MPU9250 IMU;
+    // This will print a message verifying the MPU9250 is connected
+    IMU.isConnectedMPU9250();
+
+    // Initialize the motor and IMU
 
     // Initialize motor PWM and direction pins
     // Motor PWM pin
@@ -125,51 +69,36 @@ void setup()
     // Motor direction pin
     pinMode(A0, OUTPUT);
     
-    // Initialize motor encoder pins
-    // ENCA
-    pinMode(PA15,INPUT);
-    //ENCB
-    pinMode(PA14, INPUT);
-
-    // Initialize IMU pins
-    // SDA pin
-    pinMode(PA15, INPUT);
-    // SDL pin
-    pinMode(PA14, INPUT);
+    // // Initialize IMU pins
+    // // SDA pin
+    // pinMode(PA15, INPUT);
+    // // SDL pin
+    // pinMode(PA14, INPUT);
     
 
 
-// Dfldjf
+// Create the tasks as shown in the task diagram
+
     xTaskCreate (PWM,
-                 "PWM Output",                 // Name for printouts
-                 1536,                            // Stack size
-                 NULL,                            // Parameters for task fn.
-                 4,                               // Priority
-                 NULL);                           // Task handle
-    xTaskCreate (PID,
-                 "PWM Output",                 // Name for printouts
-                 1536,                            // Stack size
-                 NULL,                            // Parameters for task fn.
-                 5,                               // Priority
-                 NULL);                           // Task handle
-    xTaskCreate (Calculator,
-                 "PWM Output",                 // Name for printouts
+                 "PWM Output to Motor",           // Name for printouts
                  1536,                            // Stack size
                  NULL,                            // Parameters for task fn.
                  3,                               // Priority
                  NULL);                           // Task handle
-    xTaskCreate (Encoder,
-                 "PWM Output",                 // Name for printouts
+    xTaskCreate (PID,
+                 "PID Outputs PWM",               // Name for printouts
                  1536,                            // Stack size
                  NULL,                            // Parameters for task fn.
                  2,                               // Priority
                  NULL);                           // Task handle
+                        // Task handle
     xTaskCreate (IMU,
-                 "PWM Output",                 // Name for printouts
+                 "Collects cube rotation",        // Name for printouts
                  1536,                            // Stack size
                  NULL,                            // Parameters for task fn.
                  1,                               // Priority
-                 NULL);                           // Task handle                 
+                 NULL);                           // Task handle           
+
     // If using an STM32, we need to call the scheduler startup function now;
     // if using an ESP32, it has already been called for us
 
@@ -179,6 +108,103 @@ void setup()
 
     Serial << "Completed Initialization, please place the cube in the starting position." << endl;
 }
+
+void PWM (void* p_params)
+{
+(void)p_params;
+
+for(;;){
+  // pull the duty cycle from the share
+  duty_cycle.get(mtr_duty);
+
+  // pull the direction from the share
+  direction.get(mtr_dir);
+
+  // turn the motor according to the pulled values
+  analogWrite(A3, mtr_duty);
+  digitalWrite(A0, mtr_dir);
+
+  // update the PID output manually
+  void IMU();
+}
+
+
+}
+void PID ( void* p_params)
+{
+(void)p_params;
+
+// Create a PID object and set Kp, Ki, Kd, and feed forward, respectively
+MiniPID PID(P,I,D,F);
+
+// Initialize PID
+// We can saturate the result from the integral component if we want to eliminate jerky response at beginning
+
+// Assign P,I,D, and F values to controller
+PID.setPID(P,I,D,F);
+// Assign the setpoint where the cube is standing
+PID.setSetpoint(VERTICAL);
+// Saturate output to the maximum duty cycle
+PID.setOutputLimits(MAX_DUTY);
+
+for(;;) {
+    
+
+
+  // get the current angle of the cube
+  cube_angle.get(cube_ang);
+
+  // get the output of the PID and put it in the share
+  mtr_duty = PID.getOutput(cube_ang, VERTICAL);
+  duty_cycle.put(mtr_duty);
+
+  error = cube_ang - VERTICAL;
+
+    // turn the motor in the correct direction to keep it balanced
+    if (error < 1){
+      direction.put(1);
+    }
+    else{
+      direction.put(0);
+    }
+
+    // Update the PWM signal manually
+    void PWM();
+}
+
+
+
+}
+void IMU(void* p_params)
+{
+(void)p_params;
+// Create a class object
+
+// MPU9250 IMU;
+
+// // The setup method calibrates and intitializes the sensor with the given memory address
+// IMU.setup(0x68);
+
+for(;;){
+
+// collect the newest data
+IMU.update();
+
+// calculate the updated angle data
+IMU.getRoll();
+IMU.getPitch();
+IMU.getYaw();
+
+// not sure which angle we are looking for, but stuff it in the share and pass it on
+cube_angle.put(IMU.getYaw());
+
+void PID();
+
+}
+}
+
+
+
 
 
 
