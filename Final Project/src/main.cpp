5cff575615b848c373d3acfd3e589951bf749f8b
Shbushka <connor.bush97@gmail.com>
2020-11-21 16:46:44 -0800

    Added doxygen style comments
    ^

--- Final Project/src/main.cpp
+++ Final Project/src/main.cpp
@@ -1,4 +1,20 @@
+/** @file main.cpp
+ *    This file contains a program that executes a series of tasks to balance a cube
+ *    on one edge using an MPU9250 accelerometer and NUCLEO STM32L476RG microcontroller.
+ *    
+ *
+ *  @author  Connor Bush
+ *  @author  Joseph Heald
+ *  @author  Blake Carbonneau
+ *  @author  Giovanni Guerrero
+ * 
+ *  @date    15 NOV 2020 Original file
+ *  @date    16 NOV 2020 Updated driver for MPU9250 and included MiniPID
+ *  @date    21 NOV 2020 Added doxygen comments for easier use and grading
+ *  @date    XX NOV 2020 Updated code after testing the physical cube
+ */
 
+// Include the necessary header files
 #include <Arduino.h>
 #include <Wire.h>
 #include <math.h>
@@ -17,18 +33,16 @@
 #include <baseshare.h>
 #include <task.h>
 
-// define general variables to be used when pulling data from shares
-// bool one_bit;
-// float float_val;
-// uint16_t sixteen_bit;
 
-// PID, PWM and IMU Variables
+// Variables that will serve as temporaty storage before passing into shares
 uint8_t mtr_duty;
 bool mtr_dir;
 float cube_ang;
-// uint16_t imu;
-// uint16_t generic_speed;
+
+// Variable that is used to determine direction of motor
 uint16_t error;
+
+// Variables used to initialize the PID controller
 uint8_t MAX_DUTY = 255; // Maximum duty cycle output
 double VERTICAL = 45; // constant value set to zero the gyro on the vertical axis
 double P = 1; // Proportional constant
@@ -39,24 +53,26 @@ double F = 0; // Feed forward value
 // Shares and queues go here
 Share<uint8_t> duty_cycle ("Motor duty cycle");
 Share<bool> direction ("Motor direction");
-// Share<uint16_t> gyro_data ("Current Cube Rotation");
-// Share<uint16_t> motor_speed("Current Motor Speed");
-// Share<uint16_t> cube_speed("Current Cube Speed");
 Share<float> cube_angle ("Current cube angle off horizontal");
-// Share<uint8_t> state ("Current state of the ");
 
 // Instantiate a MPU9250 object with a ridiculous name 
 MPU9250 MPUPU;
+
 // Instantiate MiniPID object and set Kp, Ki, Kd, and feed forward, respectively
 MiniPID PID(P,I,D,F);
 
 
-
+/** @brief   Function that runs once to setup the balancing cube.
+ *  @details This task runs once and does a number of things
+ *           1) Verifies the MPU is connected and the PID is correctly set up. 
+ *           2) Sets up motor direction and PWM pins
+ *           3) Defines the tasks used to control the cube
+ *          
+ *           If setup is completed succesfully, 5 messages will be printed. 
+ *           Make sure to read all messages before proceeding. 
+ */
 void setup() 
 {
-
-  // put your setup code here, to run once:
-  
     // Start the serial port, wait a short time, then say hello. Use the
     // non-RTOS delay() function because the RTOS hasn't been started yet
     Serial.begin (115200);
@@ -73,6 +89,8 @@ void setup()
 
     // Initialize PID
     // We can saturate the result from the integral component if we want to eliminate jerky response at beginning
+    // but I'm not sure we need to do this. We actually might want the jerky at the beginning
+    //
     // Assign P,I,D, and F values to controller
     PID.setPID(P,I,D,F);
     // Assign the setpoint where the cube is standing
@@ -80,7 +98,7 @@ void setup()
     // Saturate output to the maximum duty cycle
     PID.setOutputLimits(MAX_DUTY);
 
-    // Initialize the motor and IMU
+    // Initialize the motor and IMU pins
     // Motor PWM pin
     pinMode(A3, OUTPUT);
     // Motor direction pin
@@ -125,6 +143,12 @@ void setup()
     Serial << "Completed Initialization, please place the cube in the starting position." << endl;
 }
 
+/** @brief   Task which drives the motor. 
+ *  @details This task runs at precise intervals using @c vTaskDelay() and @c pdMS_TO_TICKS to 
+ *           set delays of intervals in ms isntead of timer ticks. This task collects the values in 
+ *           the duty_cycle and direction shares and outputs them to the motor.
+ *  @param   p_params A pointer to function parameters which we don't use.
+ */
 void PWM (void* p_params)
 {
 (void)p_params;
@@ -146,6 +170,14 @@ for(;;){
 
 
 }
+
+/** @brief   Task which runs the PID controller
+ *  @details This task runs at precise intervals using @c vTaskDelay() and @c pdMS_TO_TICKS to 
+ *           set delays of intervals in ms isntead of timer ticks. Using the angle in the share 
+ *           as the current value, the PID controller generates an output that is put into the 
+ *           mtr_duty share. The direction is decided after and put into the direction share. 
+ *  @param   p_params A pointer to function parameters which we don't use.
+ */
 void PID ( void* p_params)
 {
 (void)p_params;
@@ -178,6 +210,14 @@ for(;;) {
 
 
 }
+
+/** @brief   Task which checks button status.
+ *  @details This task runs at precise intervals using @c vTaskDelay() and @c pdMS_TO_TICKS to 
+ *           set delays of intervals in ms isntead of timer ticks. This task updates the IMU data
+ *           and then collects all three angles of rotation. The correct angle is selected and placed
+ *           into the cube_angle share.
+ *  @param   p_params A pointer to function parameters which we don't use.
+ */
 void IMU(void* p_params)
 {
 (void)p_params;
